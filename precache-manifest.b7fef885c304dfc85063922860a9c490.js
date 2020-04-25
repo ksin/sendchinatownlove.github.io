47307fd3ca8270a0eb3732742a15167fafd86422
ksin <ksin@users.noreply.github.com>
2020-04-25 00:48:48 +0000

    deploy: 2168969669fdfce185590e76953e90e41985f32b

---
+++ precache-manifest.b7fef885c304dfc85063922860a9c490.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e270510dfc43b3d554e8454f7c82d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788087f8d0143983e630",
+    "url": "/static/css/2.52f00974.chunk.css"
+  },
+  {
+    "revision": "cd6f5d7ff3b83d14047b",
+    "url": "/static/css/main.ca6d25ca.chunk.css"
+  },
+  {
+    "revision": "788087f8d0143983e630",
+    "url": "/static/js/2.86ae4a71.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.86ae4a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6f5d7ff3b83d14047b",
+    "url": "/static/js/main.42ea09e9.chunk.js"
+  },
+  {
+    "revision": "b991e9c21dba036bc800",
+    "url": "/static/js/runtime-main.f7beece5.js"
+  },
+  {
+    "revision": "f9e716bafd5bcd4ef121111701cb48c5",
+    "url": "/static/media/chinatown-logo.f9e716ba.png"
+  },
+  {
+    "revision": "4a2be72851ef3a703d4ed8cd4f1c3bd1",
+    "url": "/static/media/chinatown-splash.4a2be728.png"
+  },
+  {
+    "revision": "67dd739116e104ca62265f818c797ad2",
+    "url": "/static/media/chinatown-splash.67dd7391.png"
+  },
+  {
+    "revision": "46412c79a0082e73eb711911bf68237d",
+    "url": "/static/media/female.46412c79.svg"
+  },
+  {
+    "revision": "909f1479b519e732b5a828cc5a69317a",
+    "url": "/static/media/logo.909f1479.svg"
+  },
+  {
+    "revision": "7e1a70b6774c5a168940bee9068ea1a0",
+    "url": "/static/media/misc-store.7e1a70b6.png"
+  },
+  {
+    "revision": "6758359b8d459f6cfa6d8a0629b71881",
+    "url": "/static/media/temp.6758359b.png"
+  }
+]);
